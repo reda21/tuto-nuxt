2c3d42c29351684daffe40115154ebcf83fc7cdb
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-06-05 11:01:07 +0100

    update plugins

--- Plugins/hello.ts
+++ Plugins/hello.ts
@@ -1,22 +1,40 @@
 export default defineNuxtPlugin({
-    name: 'my-plugin',
-    enforce: 'pre', // or 'post'
-    async setup (nuxtApp) {
-        console.info('hello plugins setup');
-      // this is the equivalent of a normal functional plugin
+  name: "my-plugin",
+  enforce: "pre", // or 'post'
+  async setup(nuxtApp) {
+    console.info("hello plugins setup");
+    // this is the equivalent of a normal functional plugin
+  },
+  hooks: {
+    // You can directly register Nuxt app runtime hooks here
+    "app:created"() {
+      const nuxtApp = useNuxtApp();
+      // do something in the hook
+      logTime("hello plugins app:created")
     },
-    hooks: {
-      // You can directly register Nuxt app runtime hooks here
-      'app:created'() {
-        const nuxtApp = useNuxtApp()
+    "vue:setup"() {
+      const nuxtApp = useNuxtApp();
+      // do something in the hook
+      logTime("hello plugins vue:setup")
+    },
+    "page:start"(){
+      const nuxtApp = useNuxtApp();
+      // do something in the hook
+      logTime("hello plugins page:start")
+    },
+    "page:finish"(){
+        const nuxtApp = useNuxtApp();
         // do something in the hook
-        console.info('hello plugins app:created');
-        console.warn("tester")
-      }
-    },
-    env: {
-      // Set this value to `false` if you don't want the plugin to run when rendering server-only or island components.
-      islands: true
-    }
-  })
-  
+        logTime("hello plugins page:start")
+      },
+  },
+  env: {
+    // Set this value to `false` if you don't want the plugin to run when rendering server-only or island components.
+    islands: true,
+  },
+});
+
+
+const logTime = (msg: string) => {
+    console.info(`${msg} at ${new Date().toISOString()}`)
+}

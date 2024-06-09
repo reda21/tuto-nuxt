738c0eeba3ca0d1d4f48d7185da6a4645ded4f9b
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-06-09 13:17:42 +0100

    ajout flex.vue page

---
+++ pages/flex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Tuto flex</h1>
+    <div class="mere">
+        <div class="bg-blue-500">alpha 01</div>
+        <div class="bg-green-500">alpha 02</div>
+        <div class="bg-orange-500">alpha 03</div>
+    </div>
+    section                         
+  </div>
+</template>
+
+<style scoped>
+.mere{
+    
+}
+</style>
